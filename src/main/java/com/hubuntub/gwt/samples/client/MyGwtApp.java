bc4a313e892ed9c97bd9a36cd7ae758f8f941c53
hubuntub <begona26@gmail.com>
2014-07-20 16:16:26 +0200

    inital commit

---
+++ src/main/java/com/hubuntub/gwt/samples/client/MyGwtApp.java
@@ -0,0 +1,38 @@
+package com.hubuntub.gwt.samples.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class MyGwtApp implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+
+	private final Messages messages = GWT.create(Messages.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+		// Add the nameField and sendButton to the RootPanel
+		// Use RootPanel.get() to get the entire body element
+		VerticalPanel mainPanel = new VerticalPanel();
+		mainPanel.setWidth("100%");
+		Button button = new Button(messages.sendButton());
+		mainPanel.add(button);
+		RootPanel.get("mainContainer").add(mainPanel);
+
+	}
+}
